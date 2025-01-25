af21aa7c635b1520e0bf1fb162afba4842c8e3dc
Tobias Thiemann <tobithieman@gmail.com>
2025-01-25 11:53:01 +0100

    update to paho-mqtt 2.1.0

--- roombapy/remote_client.py
+++ roombapy/remote_client.py
@@ -122,7 +122,10 @@ class RoombaRemoteClient:
         self.mqtt_client.loop_start()
 
     def _get_mqtt_client(self) -> mqtt.Client:
-        mqtt_client = mqtt.Client(client_id=self.blid)
+        mqtt_client = mqtt.Client(
+            callback_api_version=mqtt.CallbackAPIVersion.VERSION1,
+            client_id=self.blid
+            )
         mqtt_client.username_pw_set(username=self.blid, password=self.password)
         mqtt_client.on_connect = self._internal_on_connect
         mqtt_client.on_disconnect = self._internal_on_disconnect
